f502882b1347e66b4c3609ca36b7e7ec62af03e2
Edwinmillan20 <59656611+Edwinmillan20@users.noreply.github.com>
2020-07-26 20:03:39 -0400

    Ultima actualizacion

---
+++ Asignaciones/Proyecto_1/Js/Login.js
@@ -0,0 +1,9 @@
+function go()
+{
+    if(document.form.password.value == 'ADMIN1' && document.form.login.value == 'ADMIN'){
+        document.form.submit();
+    }
+    else{
+        alert("Ingrese un Nombre de usuario y Contraseña validos");
+    }
+}
